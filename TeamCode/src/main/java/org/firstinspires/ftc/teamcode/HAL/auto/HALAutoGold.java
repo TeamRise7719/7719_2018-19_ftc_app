7c01cb96dba384e265122899575ccc496ba12e4a
emcloughlin <emcloughlin42@gmail.com>
2018-10-31 17:34:38 -0400

    AUto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAL/auto/HALAutoGold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAL/auto/HALAutoGold.java
@@ -1,7 +1,35 @@
 package org.firstinspires.ftc.teamcode.HAL.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-public class HALAutoGold extends LinearOpMode{
-    waitForStart();
-    
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.subSystems.Driving.autonomous.encoderLibrary;
+
+
+
+public class HALAutoGold extends LinearOpMode {
+
+
+    encoderLibrary enc;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        enc = new encoderLibrary(hardwareMap, telemetry,this);
+        enc.init();
+
+        waitForStart();
+
+        enc.gyroDrive(enc.DRIVE_SPEED_SLOW, 20, 0, false);
+
+
+
+
+    }
 }
