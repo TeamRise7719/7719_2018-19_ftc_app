ffe0dd0d3445ab1049fdff9cb8cb274531b16235
emcloughlin <emcloughlin42@gmail.com>
2019-01-15 20:52:33 -0500

    Created the class HAL_Articulator which is now where everything pertaining to the arm/shoulder/wrist/intake now lives

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAL/teleOp/HALteleOp3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HAL/teleOp/HALteleOp3.java
@@ -10,19 +10,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.subSystems.Driving.teleOp.HAL_Articulator;
 import org.firstinspires.ftc.teamcode.subSystems.Driving.teleOp.mecanumDrivetrain;
 
 @TeleOp(name = "HAL TeleOp3", group = "HAL")
 public class HALteleOp3 extends OpMode {
 
     private mecanumDrivetrain robot;
+    private HAL_Articulator scoring;
     Servo lift1, lift2, lift3, lift4;
-    DcMotor shoulderL;
-    DcMotor shoulderR;
-    DcMotor armL, armR;
-    CRServo wristR, wristL, intR, intL, hook;
-
-    int targetR, targetL;
+    CRServo hook;
 
 
     Telemetry tele;
@@ -33,50 +30,18 @@ public class HALteleOp3 extends OpMode {
 
         robot = new mecanumDrivetrain(hardwareMap, telemetry);
         robot.runUsingEncoders();
+        scoring = new HAL_Articulator(hardwareMap, telemetry);
         lift1 = hardwareMap.servo.get("lift1");
         lift2 = hardwareMap.servo.get("lift2");
         lift3 = hardwareMap.servo.get("lift3");
         lift4 = hardwareMap.servo.get("lift4");
 
-
         hook = hardwareMap.crservo.get("hook");
 
-
-        intR = hardwareMap.crservo.get("intR");
-        intL = hardwareMap.crservo.get("intL");
-
-
-        wristR = hardwareMap.crservo.get("wristR");
-        wristL = hardwareMap.crservo.get("wristL");
-
-
-        shoulderL = hardwareMap.dcMotor.get("shoulderL");
-        shoulderR = hardwareMap.dcMotor.get("shoulderR");
-        shoulderL.setDirection(DcMotorSimple.Direction.FORWARD);
-        shoulderR.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        armL = hardwareMap.dcMotor.get("armL");
-        armR = hardwareMap.dcMotor.get("armR");
-        armL.setDirection(DcMotorSimple.Direction.FORWARD);
-        armR.setDirection(DcMotorSimple.Direction.REVERSE);
-        armR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-        targetR = armR.getCurrentPosition();
-        targetL = armL.getCurrentPosition();
-
-        hook.setPower(1);
-
-        lift1.setPosition(0.25);
-        lift1.setPosition(0.25);
-        lift2.setPosition(0.25);
-        lift3.setPosition(0.25);
-        lift4.setPosition(0.25);
+        lift1.setPosition(0.21);
+        lift2.setPosition(0.21);
+        lift3.setPosition(0.21);
+        lift4.setPosition(0.21);
 
 
     }
@@ -95,6 +60,10 @@ public class HALteleOp3 extends OpMode {
 
         //------------------------------------=+(Drivetrain)+=------------------------------------\\
 
+
+        scoring.articulator(gamepad2);
+
+
         if (gamepad1.right_bumper) {
             lift1.setPosition(0.8);
             lift2.setPosition(0.8);
@@ -110,99 +79,5 @@ public class HALteleOp3 extends OpMode {
         if (gamepad1.b) {
             hook.setPower(-1);
         }
-
-
-        shoulderR.setPower(gamepad2.left_stick_y);
-        shoulderL.setPower(gamepad2.left_stick_y);
-
-
-
-        if (gamepad2.right_stick_y != 0) {
-
-            armR.setPower(gamepad2.right_stick_y / 2);
-            armL.setPower(gamepad2.right_stick_y / 2);
-
-            targetR = armR.getCurrentPosition();
-            targetL = armL.getCurrentPosition();
-
-        } else if (gamepad2.right_stick_y == 0 && (targetR != armR.getCurrentPosition() || targetL != armL.getCurrentPosition())) {
-
-                if (targetR - armR.getCurrentPosition() > 0) {
-                    if (targetL - armL.getCurrentPosition() > 0) {
-
-                        armR.setPower(-.5);
-                        armL.setPower(-.5);
-
-                    } else if (targetL - armL.getCurrentPosition() < 0) {
-
-                        armR.setPower(-.5);
-                        armL.setPower(.5);
-
-                    }
-                } else if (targetR - armR.getCurrentPosition() < 0) {
-                    if (targetL - armL.getCurrentPosition() > 0) {
-
-                        armR.setPower(.5);
-                        armL.setPower(-.5);
-
-                    } else if (targetL - armL.getCurrentPosition() < 0) {
-
-                        armR.setPower(.5);
-                        armL.setPower(.5);
-
-                    }
-
-                }
-        } else {
-
-            armR.setPower(0);
-            armL.setPower(0);
-
         }
-
-
-
-
-
-//        telemetry.addData("armR", armR.getCurrentPosition());
-//        telemetry.addData("armL", armL.getCurrentPosition());
-
-            if (gamepad2.a) {
-                wristL.setPower(0.25);
-                wristR.setPower(-0.25);
-            } else if (gamepad2.y) {
-                wristL.setPower(-0.25);
-                wristR.setPower(0.25);
-            }
-
-
-            if (gamepad2.x) {
-                armR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                armL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            }
-
-//        if (armR.getTargetPosition()  >= 4200 || armL.getTargetPosition() >= 4200){
-//          Math.abs(gamepad2.right_stick_y * -1);
-//
-//        } else if (armR.getTargetPosition()  <= 150 || armL.getTargetPosition() <=150){
-//          Math.abs(gamepad2.right_stick_y);
-//        }
-
-
-//        if (gamepad2.dpad_down){
-//          intL.setPower(0.2);
-//          intR.setPower(-0.2);
-//        }
-//        if (gamepad2.dpad_up){
-//            intL.setPower(0.8);
-//            intR.setPower(-0.8);
-//
-//            intL.setPower(gamepad2.left_stick_y);
-//            intR.setPower(-gamepad2.left_stick_y);
-//        }
-
-
-        }
-
-
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/Driving/teleOp/HAL_Articulator.java
@@ -0,0 +1,230 @@
+package org.firstinspires.ftc.teamcode.subSystems.Driving.teleOp;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class HAL_Articulator {
+
+    private final HardwareMap hardwareMap;
+    private final Telemetry telemetry;
+
+    private final DcMotor shoulderL, shoulderR, armL, armR;
+    private final CRServo wristR, wristL, intR, intL;
+
+    private final AnalogInput shoulderPot;
+    double potMin = 0, potMax = 2000;
+
+    int targetR, targetL;
+
+
+
+    public HAL_Articulator(final HardwareMap _hardwareMap, final Telemetry _telemetry) {
+
+        hardwareMap = _hardwareMap;
+        telemetry = _telemetry;
+
+        intR = hardwareMap.crservo.get("intR");
+        intL = hardwareMap.crservo.get("intL");
+
+
+        wristR = hardwareMap.crservo.get("wristR");
+        wristL = hardwareMap.crservo.get("wristL");
+
+
+        shoulderL = hardwareMap.dcMotor.get("shoulderL");
+        shoulderR = hardwareMap.dcMotor.get("shoulderR");
+        shoulderL.setDirection(DcMotorSimple.Direction.FORWARD);
+        shoulderR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        armL = hardwareMap.dcMotor.get("armL");
+        armR = hardwareMap.dcMotor.get("armR");
+        armL.setDirection(DcMotorSimple.Direction.FORWARD);
+        armR.setDirection(DcMotorSimple.Direction.REVERSE);
+        armR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        shoulderPot = hardwareMap.analogInput.get("shoulderPot");
+
+
+        targetR = armR.getCurrentPosition();
+        targetL = armL.getCurrentPosition();
+    }
+
+    private void arm(Gamepad gamepad2){
+
+        if (gamepad2.right_stick_y != 0) {
+
+            armR.setPower(gamepad2.right_stick_y / 2);
+            armL.setPower(gamepad2.right_stick_y / 2);
+
+            targetR = armR.getCurrentPosition();
+            targetL = armL.getCurrentPosition();
+
+        } else if (gamepad2.right_stick_y == 0 && (targetR != armR.getCurrentPosition() || targetL != armL.getCurrentPosition())) {
+
+            if (targetR - armR.getCurrentPosition() > 0) {
+                if (targetL - armL.getCurrentPosition() > 0) {
+
+                    armR.setPower(-.5);
+                    armL.setPower(-.5);
+
+                } else if (targetL - armL.getCurrentPosition() < 0) {
+
+                    armR.setPower(-.5);
+                    armL.setPower(.5);
+
+                }
+            } else if (targetR - armR.getCurrentPosition() < 0) {
+                if (targetL - armL.getCurrentPosition() > 0) {
+
+                    armR.setPower(.5);
+                    armL.setPower(-.5);
+
+                } else if (targetL - armL.getCurrentPosition() < 0) {
+
+                    armR.setPower(.5);
+                    armL.setPower(.5);
+
+                }
+
+            }
+        } else {
+
+            armR.setPower(0);
+            armL.setPower(0);
+
+        }
+
+    }
+
+    private void shoulder(Gamepad gamepad2){
+
+        // TO BE IMPLEMENTED IMMEDIATELY
+
+//        if (shoulderPot.getVoltage() < potMin) {
+//
+//            if (gamepad2.left_stick_y < 0){
+//
+//                shoulderR.setPower(0);
+//                shoulderL.setPower(0);
+//
+//            } else {
+//
+//                shoulderR.setPower(gamepad2.left_stick_y);
+//                shoulderL.setPower(gamepad2.left_stick_y);
+//
+//            }
+//
+//        } else if (shoulderPot.getVoltage() > potMin){
+//
+//            if (gamepad2.left_stick_y > 0){
+//
+//                shoulderR.setPower(0);
+//                shoulderL.setPower(0);
+//
+//            } else {
+//
+//                shoulderR.setPower(gamepad2.left_stick_y);
+//                shoulderL.setPower(gamepad2.left_stick_y);
+//
+//            }
+//
+//        } else {
+//
+//            shoulderR.setPower(gamepad2.left_stick_y);
+//            shoulderL.setPower(gamepad2.left_stick_y);
+//
+//        }
+
+        shoulderR.setPower(gamepad2.left_stick_y);
+        shoulderL.setPower(gamepad2.left_stick_y);
+
+    }
+
+    private void wrist(Gamepad gamepad2){
+
+        if (gamepad2.a) { // Intake position
+
+            wristL.setPower(0.25);
+            wristR.setPower(-0.25);
+
+        } else if (gamepad2.y) { // Up (rest) position
+
+            wristL.setPower(-0.25);
+            wristR.setPower(0.25);
+
+        } else if (gamepad2.b) { // Scoring position
+
+            wristL.setPower(-0.25);
+            wristR.setPower(0.25);
+
+        }
+
+        //This is for minor, on the fly adjustments
+
+        if (gamepad2.dpad_up) {
+
+            wristL.setPower(wristL.getPower() + 0.05);
+            wristR.setPower(wristR.getPower() - 0.05);
+
+        } else if (gamepad2.dpad_down) {
+
+            wristL.setPower(wristL.getPower() - 0.05);
+            wristR.setPower(wristR.getPower() + 0.05);
+
+        }
+
+    }
+
+    private void intake(Gamepad gamepad2){
+
+        if((gamepad2.right_trigger < 0.25) && (gamepad2.right_trigger > 0)){
+
+            intL.setPower(0.25);
+            intR.setPower(-0.25);
+
+        } else if (gamepad2.right_trigger > 0.8) {
+
+            intL.setPower(0.8);
+            intR.setPower(-0.8);
+
+        } if((gamepad2.left_trigger < 0.25) && (gamepad2.left_trigger > 0)){
+
+            intL.setPower(-0.25);
+            intR.setPower(0.25);
+
+        } else if (gamepad2.left_trigger > 0.8) {
+
+            intL.setPower(-0.8);
+            intR.setPower(0.8);
+
+        } else {
+
+            intL.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+            intR.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+        }
+
+    }
+
+    public void articulator(Gamepad gamepad2){
+
+        arm(gamepad2);
+        shoulder(gamepad2);
+        wrist(gamepad2);
+        intake(gamepad2);
+
+    }
+
+}
